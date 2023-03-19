89b2edfc46b9e9256561edef09f724e694d20af4
Daxin Wang <daxinwang@harmonycloud.cn>
2023-03-19 21:37:22 +0800

    Don't start the collector if the kernel is not supported
    Signed-off-by: Daxin Wang <daxinwang@harmonycloud.cn>

--- collector/docker/start.sh
+++ collector/docker/start.sh
@@ -3,6 +3,9 @@
 
 if [ -f "/opt/probe.o" ]; then
 	export SYSDIG_BPF_PROBE="/opt/probe.o"
+	/app/kindling-collector --config=/app/config/kindling-collector-config.yml
+else
+  echo "The kernel is not supported natively."
+  echo "Please read FAQ http://kindling.harmonycloud.cn/docs/installation/faq/#error-precompiled-module-at-optkindling-is-not-found"
+  sleep infinity
 fi
-
-/app/kindling-collector --config=/app/config/kindling-collector-config.yml
